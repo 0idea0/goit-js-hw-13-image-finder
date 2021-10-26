b30bbf05e1dc289f93c1fa03d047f5ac4a07b3d0
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2021-10-26 14:37:18 +0300

    add infinity load

---
+++ src/js/plugins/infinity-load.js
@@ -0,0 +1,19 @@
+import { galleryRef } from '../refs.js';
+
+export default function infinityLoad(render) {
+  const onEntry = entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        render();
+        observer.unobserve(galleryRef.lastElementChild); // продолжает следить
+        //  observer.disconnect(galleryRef.lastElementChild); // отменяет слежение
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(onEntry, {
+    rootMargin: '50px',
+  });
+
+  observer.observe(galleryRef.lastElementChild);
+}
